e95be131102b2f1757054d5ef5be5fa280e9633c
danialbleile <danial.bleile@wsu.edu>
2019-06-25 13:55:13 -0700

    add css base

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,43 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-	});
+
+		sass: {
+			components: {
+				options: {
+					sourcemap: 'none',
+				},
+				files: [{
+					expand: true,
+					cwd: "components",
+					src: ["**/*.scss"],
+					dest: "components",
+					ext: ".css",
+				}]
+			},
+			prod: {
+				options: {
+					sourcemap: 'none',
+				},
+				files: [{
+					expand: true,
+					cwd: "css",
+					src: ["**/*.scss"],
+					dest: "css",
+					ext: ".css",
+				}]
+			},
+		},
+		
+	})
 
 	// Load Grunt plugins
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass')
+	grunt.loadNpmTasks('grunt-postcss')
+	grunt.loadNpmTasks('grunt-contrib-cssmin')
+	grunt.loadNpmTasks('grunt-contrib-uglify')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-contrib-jshint')
 
 	// Register Grunt tasks
 	grunt.registerTask('default', ['watch']);
