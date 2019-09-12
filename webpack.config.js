40b9726d377f3f5ad24632bc8a325a95f8102c26
danialbleile <danial.bleile@wsu.edu>
2019-09-12 16:18:57 -0700

    Setup wepack to compile scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,52 @@
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
 	context: __dirname,
 	entry: {
-		'components/global-header': './components/global-header/global-header.js',
+        //scripts: './components/global-header/global-header.js',
+        'components/global-header/global-header': './components/global-header/build/global-header.scss',
 	},
 	output: {
-        path: path.resolve(__dirname, './'),
-        filename: '[name].js'
-    },
+		path: path.resolve(__dirname, './'),
+		filename: '[name]_bundle.js'
+	},
 	mode: 'development',
-    devtool: 'source-map',
-    
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: /.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+					},
+					{
+						loader: "postcss-loader",
+						options: {
+							ident: "postcss",
+							plugins: () => [
+								autoprefixer({
+									overrideBrowserslist: [
+										">1%",
+										"last 4 versions",
+										"Firefox ESR",
+										"not ie < 9"
+									]
+								})
+							]
+						}
+					},
+					"sass-loader"
+				],
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	]
 };
