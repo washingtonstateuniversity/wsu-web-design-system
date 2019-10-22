3955f4e4aca8106588eee9433bc828028d3d5d99
Nic Ford <nicdford@gmail.com>
2019-10-22 11:01:56 -0700

    Setup build/dev process for webpack config

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WSU-Web-Design-System
+
+## Local Development
+- For local development of components: `npm run start`
+- For building component parts (html, css, js): `npm run build:parts`
+- TODO: Add ability to build for packages (global, site, content, etc.): `npm run build:packages`
+- TODO: Add ability to build all (packages and parts): `npm run build:all`
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,101 @@
 const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-	context: __dirname,
-	entry: {
+module.exports = env => {
+
+	const DEV = env.NODE_ENV === 'development';
+
+	return {
+		context: __dirname,
+		entry: {
 			'components/dist/wsu-reset/wsu-reset': './components/src/wsu-reset/index.js',
-		'components/dist/global-header/global-header': './components/src/global-header/index.js',
-		'components/dist/global-footer/global-footer': './components/src/global-footer/index.js',
-	},
-	output: {
-		path: path.resolve(__dirname),
-		filename: '[name].js'
-	},
-	mode: 'development',
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /.scss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{
-						loader: "css-loader",
-					},
-					{
-						loader: "postcss-loader",
-						options: {
-							ident: "postcss",
-							plugins: () => [
-								autoprefixer({
-									overrideBrowserslist: [
-										">1%",
-										"last 4 versions",
-										"Firefox ESR",
-										"not ie < 9"
-									]
-								})
-							]
+			'components/dist/global-footer/global-footer': './components/src/global-footer/index.js',
+			'components/dist/global-header/global-header': './components/src/global-header/index.js',
+		},
+		output: {
+			path: path.resolve(__dirname),
+			filename: '[name].js'
+		},
+		mode: DEV ? 'development' : 'production',
+		devtool: DEV ? 'inline-source-map' : 'source-map',
+		devServer: {
+			writeToDisk: true,
+			contentBase: path.join(__dirname, 'components/dist'),
+			overlay: true,
+			quiet: true
+		},
+		module: {
+			rules: [
+				{
+					test: /.scss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						{
+							loader: "css-loader",
+						},
+						{
+							loader: "postcss-loader",
+							options: {
+								ident: "postcss",
+								plugins: () => [
+									autoprefixer({
+										overrideBrowserslist: [
+											">1%",
+											"last 4 versions",
+											"Firefox ESR",
+											"not ie < 9"
+										]
+									})
+								]
+							}
+						},
+						"sass-loader"
+					],
+				},
+				{
+					test: /.html$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: 'components/dist/[name]/[name].[ext]',
+							}
+						},
+					],
+				},
+			],
+		},
+		optimization: {
+			minimize: !DEV,
+			minimizer: [
+				new OptimizeCSSAssetsPlugin({
+					cssProcessorOptions: {
+						map: {
+							inline: false,
+							annotation: true
+						}
+					}
+				}),
+				new TerserPlugin({
+					terserOptions: {
+						compress: {
+							warnings: false,
+						},
+						output: {
+							comments: false
 						}
 					},
-					"sass-loader"
-				],
-			},
-			{
-				test: /.html$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'components/dist/[name]/[name].[ext]',
-						}
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-	]
+					sourceMap: true
+				})
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: '[name].css'
+			}),
+		]
+	}
 };
