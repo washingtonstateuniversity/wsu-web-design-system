f697ed0699e151bb10e4f89ea186f1b49f747432
Nic Ford <nicdford@gmail.com>
2019-11-14 09:34:01 -0800

    Initial attempt at getting priority-nav working

--- components/src/site-nav-horizontal/site-nav-horizontal.js
+++ components/src/site-nav-horizontal/site-nav-horizontal.js
@@ -1,1 +1,5 @@
-console.log('Javascript loaded');
+
+// import { priorityNav } from 'priority-nav/dist/priority-nav';
+const priorityNav = require('priority-nav/dist/priority-nav');
+
+priorityNav.init();
