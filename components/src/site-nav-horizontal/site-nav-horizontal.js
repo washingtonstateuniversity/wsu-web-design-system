9787c1fd82c94527d37ff4aa9dbb25447e1a183c
Nic Ford <nicdford@gmail.com>
2019-11-14 17:04:18 -0800

    Setup priority-navigation and get initial toggle functionality working

--- components/src/site-nav-horizontal/site-nav-horizontal.js
+++ components/src/site-nav-horizontal/site-nav-horizontal.js
@@ -1,5 +1,38 @@
+// Priority Nav
+import priorityNav from 'priority-nav';
 
-// import { priorityNav } from 'priority-nav/dist/priority-nav';
-const priorityNav = require('priority-nav/dist/priority-nav');
+priorityNav.init({
+	mainNavWrapper: ".wsu-s-nav-horizontal__nav-container",
+	breakPoint: 0,
+	throttleDelay: 50
+});
 
-priorityNav.init();
+// Init Collapse Menus
+function setAriaExpandedFalse(elements) {
+	elements.forEach(element => {
+		element.setAttribute('aria-expanded', 'false');
+	});
+}
+
+const ariaExpandedElements = document.querySelectorAll('[aria-expanded="true"]');
+
+setAriaExpandedFalse(ariaExpandedElements);
+
+// On Click, Toggle Menus Open
+const menuLinks = document.querySelectorAll('.wsu-s-nav-horizontal__nav-link');
+
+menuLinks.forEach(link => {
+	link.addEventListener("click", function (e) {
+		e.preventDefault();
+
+		const subMenu = e.target.nextElementSibling;
+
+		if (subMenu != null) {
+			if (subMenu.hasAttribute('aria-expanded') && subMenu.getAttribute('aria-expanded') == 'false') {
+				e.target.nextElementSibling.setAttribute('aria-expanded', 'true');
+			} else {
+				e.target.nextElementSibling.setAttribute('aria-expanded', 'false');
+			}
+		}
+	});
+});
