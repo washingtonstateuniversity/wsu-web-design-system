21285d03dfa50f795900122c6986cf6d79c15562
Nic Ford <nicdford@gmail.com>
2019-11-18 17:07:33 -0800

    Setup horizontal nav to work with new build-tools priorityNav

--- components/src/site-nav-horizontal/site-nav-horizontal.js
+++ components/src/site-nav-horizontal/site-nav-horizontal.js
@@ -1,53 +1,20 @@
-/**
- * Imports
- */
-import priorityNav from 'priority-nav';
-import { Menubar } from '@wsu/build-tools/js/wai-aria/MenubarLinks';
+import priorityNav from '../../../../wsu-build-tools/js/wsu-bt-priorityNav';
+import { Menubar } from '../../../../wsu-build-tools/js/wai-aria/MenubarLinks';
+// TODO: import from NPM once final version is ready
 
 // Remove no-js class if js is present
 document.querySelector('.wsu-s-nav-horizontal__wrapper').classList.remove('wsu-u-no-js');
+
+// Set collapsible nav items to hidden
+var collapsibleItems = document.querySelectorAll('.wsu-s-nav-horizontal__nav-link[aria-expanded="true"]');
+collapsibleItems.forEach(element => {
+	element.setAttribute('aria-expanded', 'false');
+});
 
 // Initiate keyboard controls for accessibility support
 var menubar = new Menubar(document.getElementById('wsu-s-nav-horizontal'));
 menubar.init();
 
-// Priority Nav
-priorityNav.init({
-	mainNavWrapper: ".wsu-s-nav-horizontal__nav-container",
-	breakPoint: 0,
-	throttleDelay: 50,
-	navDropdownClassName: "nav__dropdown", // class used for the dropdown - this is a class name, not a selector.
-	navDropdownToggleClassName: "nav__dropdown-toggle", // class used for the dropdown toggle - this is a class name, not a selector.
-	navDropdownLabel: "more", // Text that is used for the dropdown toggle.
-	navDropdownBreakpointLabel: "menu", //button label for navDropdownToggle when the breakPoint is reached.
-});
-
-// Init Collapse Menus
-function setAriaExpandedFalse(elements) {
-	elements.forEach(element => {
-		element.setAttribute('aria-expanded', 'false');
-	});
-}
-
-const ariaExpandedElements = document.querySelectorAll('[aria-expanded="true"]');
-
-setAriaExpandedFalse(ariaExpandedElements);
-
-// On Click, Toggle Menus Open
-const menuLinks = document.querySelectorAll('.wsu-s-nav-horizontal__nav-link');
-
-menuLinks.forEach(link => {
-	link.addEventListener("click", function (e) {
-		e.preventDefault();
-
-		const subMenu = e.target.nextElementSibling;
-
-		if (subMenu != null) {
-			if (subMenu.hasAttribute('aria-expanded') && subMenu.getAttribute('aria-expanded') == 'false') {
-				e.target.nextElementSibling.setAttribute('aria-expanded', 'true');
-			} else {
-				e.target.nextElementSibling.setAttribute('aria-expanded', 'false');
-			}
-		}
-	});
-});
+// Initiate Priority Nav
+var horizontalNav = new priorityNav('.wsu-s-nav-horizontal__nav-list');
+horizontalNav.init();
