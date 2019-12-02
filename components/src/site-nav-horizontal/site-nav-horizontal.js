bf1e4b4c0dddb31fd147bdad7737fc97e4e712d1
Nic Ford <nicdford@gmail.com>
2019-12-02 14:09:02 -0800

    Various bug fixes

--- components/src/site-nav-horizontal/site-nav-horizontal.js
+++ components/src/site-nav-horizontal/site-nav-horizontal.js
@@ -17,7 +17,8 @@ var horizontal_nav = new priority_nav({
 	main_nav_selector: '.wsu-s-nav-horizontal__nav-list',
 	priority_nav_list_item_class_name: 'wsu-s-nav-horizontal__nav-item--more',
 	priority_nav_list_item_link_class_name: 'wsu-s-nav-horizontal__nav-link',
-	priority_nav_list_item_list_class_name: 'wsu-s-nav-horizontal__nav-list--has-children'
+	priority_nav_list_item_list_class_name: 'wsu-s-nav-horizontal__nav-list--has-children',
+	more_inner_html: '<svg class="wsu-s-nav-horizontal__more-button" version="1.2" baseProfile="tiny" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="107.33px" height="22px" viewBox="0 0 107.33 22" xml:space="preserve"><defs></defs><circle cx="96.33" cy="11" r="11"/><circle cx="53.67" cy="11" r="11"/><circle cx="11" cy="11" r="11"/></svg>'
 });
 horizontal_nav.init();
 
