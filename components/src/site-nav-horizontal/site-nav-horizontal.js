8d2a834d5521f26f7a2965278ccd776b9b8d6055
Nic Ford <nicdford@gmail.com>
2019-12-03 12:55:23 -0800

    Remove aria_expanded functionality from component level js file

--- components/src/site-nav-horizontal/site-nav-horizontal.js
+++ components/src/site-nav-horizontal/site-nav-horizontal.js
@@ -1,16 +1,9 @@
 // TODO: import from NPM once final version is ready
 import { Menubar } from '../../../../wsu-build-tools/js/wai-aria/MenubarLinks';
 import priority_nav from '../../../../wsu-build-tools/js/wsu-bt-priority-nav';
-import wsu_bt_aria_expanded from '../../../../wsu-build-tools/js/wsu-bt-aria-expanded';
 
 // Remove no-js class if js is present
 document.querySelector('.wsu-s-nav-horizontal__wrapper').classList.remove('wsu-u-no-js');
-
-// Initiate collapsable aria-expanded items
-var expanded_aria_items = new wsu_bt_aria_expanded({
-	nav_item_selector: '.wsu-s-nav-horizontal__nav-link'
-});
-expanded_aria_items.init();
 
 // Initiate Priority Nav
 var horizontal_nav = new priority_nav({
