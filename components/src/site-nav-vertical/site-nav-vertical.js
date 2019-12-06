faa0be678ee936d9bb89cdc94b5c049f3032cdfa
Nic Ford <nicdford@gmail.com>
2019-12-05 17:03:45 -0800

    Build out vertical nav styles and interactions

--- components/src/site-nav-vertical/site-nav-vertical.js
+++ components/src/site-nav-vertical/site-nav-vertical.js
@@ -1,1 +1,6 @@
-console.log('Javascript loaded');
+import wsu_bt_aria_expanded from '@wsu/build-tools/js/wsu-bt-aria-expanded';
+
+var expanded_aria_items = new wsu_bt_aria_expanded({
+	nav_item_selector: '.wsu-s-nav-vertical__menu-icon-link'
+});
+expanded_aria_items.init();
