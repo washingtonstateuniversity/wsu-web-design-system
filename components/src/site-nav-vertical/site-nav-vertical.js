d462ff93be52f7f32e85f1475842724fe3ba6f5d
Nic Ford <nicdford@gmail.com>
2020-01-07 09:44:42 -0800

    Move item animations into vert nav and move extra close button out

--- components/src/site-nav-vertical/site-nav-vertical.js
+++ components/src/site-nav-vertical/site-nav-vertical.js
@@ -6,30 +6,13 @@ import wsu_bt_vertical_nav from '../../../../wsu-build-tools/js/vertical-nav/wsu
  *
  */
 const vertical_nav = new wsu_bt_vertical_nav({
-	nav_selector: '.wsu-s-nav-vertical__nav-link',
+	nav_item_selector: '.wsu-s-nav-vertical__nav-link',
 	nav_panel_control_selector: '.wsu-s-nav-vertical__menu-icon-link',
-	nav_list_container_selector: '.wsu-s-nav-vertical__nav-list-container'
+	nav_panel_selector: '.wsu-s-nav-vertical__wrapper',
+	nav_list_container_selector: '.wsu-s-nav-vertical__nav-list-container',
 });
 vertical_nav.init();
 
-/**
- *
- * Animate menu items in on vert nav open using emitters
- *
- */
-const navItems = document.querySelectorAll('.wsu-s-nav-vertical__nav-list-container > li');
-const navItemsCount = navItems.length;
-
-document.emitter.on('wsu-vertical-nav--after-open', function (e) {
-	for (var i = 0; i < navItemsCount; i++) {
-		(function (i) {
-			const duration = 250;
-			let increment = duration + (duration * i);
-
-			setTimeout(function () {
-				navItems[i].style.opacity = 1;
-				navItems[i].style.marginLeft = '0';
-			}, increment);
-		})(i);
-	};
+document.querySelector('.wsu-s-nav-vertical__nav-container-close-link').addEventListener('click', function () {
+	vertical_nav.closePanel();
 });
