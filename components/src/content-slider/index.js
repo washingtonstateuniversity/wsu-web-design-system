98a4db4da92a8055f0d50d29d3198d035330deb2
Nic Ford <nicdford@gmail.com>
2020-06-03 17:13:09 -0700

    Setup storybook for slider component

--- components/src/content-slider/index.js
+++ components/src/content-slider/index.js
@@ -1,8 +1,51 @@
-// JS
-import './content-slider.js';
+import React, {Component} from 'react';
+import Swiper from 'swiper';
 
-// SCSS
 import './content-slider.scss';
 
-// HTML
-import './content-slider.html';
+class ContentSlider extends Component {
+	componentDidMount(){
+		var mySwiper = new Swiper ('.wsu-c-slider', {
+			// Optional parameters
+			direction: 'vertical',
+			loop: true,
+		
+			// If we need pagination
+			pagination: {
+			  el: '.swiper-pagination',
+			},
+		
+			// Navigation arrows
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+		
+			// And if we need scrollbar
+			scrollbar: {
+			  el: '.swiper-scrollbar',
+			},
+		});	
+	}
+
+	render() {
+		return (
+			<div className="wsu-c-slider">
+				<div className="wsu-c-slider-slide">
+					<img className="wsu-c-slider-slide_background" src="https://source.unsplash.com/random" alt="#" data-object-fit="" />
+				</div>
+				<div className="wsu-c-slider-slide">
+					<img className="wsu-c-slider-slide_background" src="https://source.unsplash.com/random" alt="#" data-object-fit="" />
+				</div>
+				<div className="wsu-c-slider-slide">
+					<img className="wsu-c-slider-slide_background" src="https://source.unsplash.com/random" alt="#" data-object-fit="" />
+				</div>
+				<div className="wsu-c-slider-slide">
+					<img className="wsu-c-slider-slide_background" src="https://source.unsplash.com/random" alt="#" data-object-fit="" />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default ContentSlider;
---
+++ components/src/content-slider/stories/index.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContentSlider from '../';
+
+export default {
+	title: 'Slider' 
+};
+
+export const DefaultSlider = () => {
+	return <ContentSlider />;
+};
+
+export const DefaultSliderDark = () => {
+	return <ContentSlider />;
+};
