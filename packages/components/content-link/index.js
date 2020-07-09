72d44389144e48b646ad80dfae1b9dbff134739b
Nic Ford <nicdford@gmail.com>
2020-07-09 09:19:56 -0700

    Create content link component

---
+++ packages/components/content-link/index.js
@@ -0,0 +1,36 @@
+// External Deps
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Component Deps
+import './content-link.scss';
+
+// Component Starts
+class ContentLink extends Component {
+	render() {
+		return (
+			<a 
+				href={this.props.href} 
+				class={'wsu-c-link ' + this.props.classNames}
+				target={this.props.target}
+			>
+				{this.props.children}
+			</a>
+		);
+	}
+}
+
+ContentLink.propTypes = {
+	href: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	target: PropTypes.string
+}
+
+ContentLink.defaultProps = {
+	href: '#',
+	classNames: '',
+	target: '_self'
+}
+
+export default ContentLink;
+
