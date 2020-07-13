9d226ece7d6f05b66b51833ba3c3e7136c7e4acb
Nic Ford <nicdford@gmail.com>
2020-07-13 13:59:20 -0700

    Setup content link component and it's stories

--- packages/components/content-link/index.js
+++ packages/components/content-link/index.js
@@ -1,6 +1,7 @@
 // External Deps
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 // Component Deps
 import './content-link.scss';
@@ -8,10 +9,19 @@ import './content-link.scss';
 // Component Starts
 class ContentLink extends Component {
 	render() {
+		const classes = classnames(
+			'wsu-c-link', 
+			{
+				'wsu-c-link--is-secondary': this.props.isSecondary,
+				'wsu-c-link--motion': this.props.isAnimated,
+				'wsu-c-link--a11y': this.props.isHighlyAccessible
+			}
+		)
+
 		return (
 			<a 
 				href={this.props.href} 
-				className={'wsu-c-link ' + this.props.classNames}
+				className={classes}
 				target={this.props.target}
 			>
 				{this.props.children}
@@ -23,13 +33,19 @@ class ContentLink extends Component {
 ContentLink.propTypes = {
 	href: PropTypes.string.isRequired,
 	className: PropTypes.string,
-	target: PropTypes.string
+	target: PropTypes.string,
+	isSecondary: PropTypes.bool,
+	isAnimated: PropTypes.bool,
+	isHighlyAccessible: PropTypes.bool
 }
 
 ContentLink.defaultProps = {
 	href: '#',
 	classNames: '',
-	target: '_self'
+	target: '_self',
+	isSecondary: false,
+	isAnimated: false,
+	isHighlyAccessible: false
 }
 
 export default ContentLink;
