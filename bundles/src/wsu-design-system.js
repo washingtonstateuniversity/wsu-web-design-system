d42b032668742d6965750da0418625de496c2cb0
Danial Bleile <danbleile@gmail.com>
2020-07-17 10:19:20 -0700

    Update Button

--- bundles/src/wsu-design-system.js
+++ bundles/src/wsu-design-system.js
@@ -5,14 +5,15 @@
  */
 
 /* Global components */
-import '../../components/src/global-footer/build';
-import '../../components/src/global-header/build';
+//import '../../components/src/global-footer/build';
+//import '../../components/src/global-header/build';
 import '../../components/src/global-colors/build';
+import '../../packages/components/content-link/content-link.scss'; // Needs to come first
 
-import '../../packages/components/containers/component.scss';
+//import '../../packages/components/containers/component.scss';
 
 /* Site components */
-import '../../components/src/site-footer/build';
+//import '../../components/src/site-footer/build';
 import '../../components/src/site-footer-condensed/build';
 import '../../components/src/site-nav-horizontal/build';
 import '../../components/src/site-nav-vertical/build';
@@ -22,17 +23,23 @@ import '../../components/src/site-header/build';
 import '../../components/src/content-article-copy/build';
 import '../../components/src/content-article-header/build';
 import '../../components/src/content-article-footer/build';
-import '../../components/src/content-button/build';
-import '../../components/src/content-card/build';
+// import '../../components/src/content-button/build';
+// import '../../components/src/content-card/build';
 import '../../components/src/content-form/build';
-import '../../components/src/content-hero/build';
-import '../../components/src/content-link/build';
+// import '../../components/src/content-hero/build';
+// import '../../components/src/content-link/build';
 import '../../components/src/content-list/build';
 
-import '../../packages/components/content-paragraph/component.scss';
-import '../../packages/components/content-column/component.scss';
-import '../../packages/components/content-heading/component.scss';
+import '../../packages/components/containers/style.scss';
+import '../../packages/components/content-paragraph/style.scss';
+import '../../packages/components/content-column/style.scss';
+import '../../packages/components/content-heading/style.scss';
 import '../../packages/components/content-hero/style.scss';
+import '../../packages/components/content-button/style.scss';
+import '../../packages/components/content-card/content-card.scss';
+import '../../packages/components/global-header/style.scss';
+import '../../packages/components/global-footer/style.scss';
+import '../../packages/components/site-footer/style.scss';
 
 
 
