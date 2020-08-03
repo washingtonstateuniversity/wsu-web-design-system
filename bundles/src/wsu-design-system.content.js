0d948b11a06191df390f7d3a909136211e43c6b1
Nic Ford <nicdford@gmail.com>
2020-08-03 16:31:52 -0700

    More house keeping

--- bundles/src/wsu-design-system.content.js
+++ bundles/src/wsu-design-system.content.js
@@ -2,8 +2,8 @@
  * All styles included in this file should be general content items
  * scoped to .wsu-c-content. Specific individual components should not be included. 
  */
-import '../../packages/components/src/content-link/build';
-import '../../packages/components/src/content-paragraph/build';
-import '../../packages/components/src/content-heading/build';
-import '../../packages/components/src/content-form/build';
-import '../../packages/components/src/content-list/build';
+import '../../packages/components/content-link/style.scss';
+import '../../packages/components/content-paragraph/style.scss';
+import '../../packages/components/content-heading/style.scss';
+import '../../packages/components/content-form/style.scss';
+import '../../packages/components/content-list/style.scss';
