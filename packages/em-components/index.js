a2d0e1236b8e7f4fede719b0dcb673be9421a049
Nic Ford <nicdford@gmail.com>
2020-08-12 14:03:05 -0700

    Setup EM table styles

--- packages/em-components/index.js
+++ packages/em-components/index.js
@@ -1,2 +1,3 @@
 export { default as EmContentCallout } from './em-content-callout';
 export { default as EmContentHeader } from './em-content-header'; 
+export { default as EmContentTable } from './em-content-table'; 
