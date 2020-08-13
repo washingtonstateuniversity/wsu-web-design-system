049ecaf3d595965d8b0d92835ffafadb5fee51ad
Nic Ford <nicdford@gmail.com>
2020-08-12 20:12:39 -0700

    Setup em ordered list component

---
+++ storybook/stories/em/content-em-ordered-list.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs, text, boolean, number, select } from "@storybook/addon-knobs";
+import { previewGroupID, optionsGroupID } from '@wsuwebteam/build-tools/js/helpers/storybook';
+
+// Storybook Imports
+import ComponentWrapper from '../../../packages/storybook-components/content-wrapper/index'
+
+// Component Imports
+import { EmOrderedList } from '../../../packages/em-components';
+
+// Story Time
+export default {
+	title: 'EM/Components/Ordered List',
+	decorators: [
+		withA11y,
+		withKnobs,
+	]
+};
+
+export const Default = () => {
+
+	return (
+		<ComponentWrapper layout="content" className="wsu-c-content">
+			<div style={{ maxWidth: "588px" }}>
+				<EmOrderedList />
+			</div>
+		</ComponentWrapper>
+	)
+}
