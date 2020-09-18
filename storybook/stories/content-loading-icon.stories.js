1cfa6318e06a5f5db5d4d5c068a6a0774a3aa961
Nic Ford <nicdford@gmail.com>
2020-09-18 16:39:33 -0700

    Finalize first draft of default loading icon

---
+++ storybook/stories/content-loading-icon.stories.js
@@ -0,0 +1,24 @@
+// External Deps
+import React from 'react';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs, text, boolean, number, select } from "@storybook/addon-knobs";
+
+// Component Deps
+import { WsuLoadingIcon } from '../../packages/components';
+
+// Storybook Imports
+import ComponentWrapper from '../../packages/storybook-components/content-wrapper';
+
+// Story Time
+export default {
+	title: 'Components/Content/Loading Icon',
+	parameters: {
+		status: 'Needs Feedback' // In Development | Needs Feedback | Stable | Needs Deployment
+	},
+	decorators: [
+		withA11y,
+		withKnobs
+	]
+};
+
+export const default = () => <ComponentWrapper layout="centered" ><WsuLoadingIcon /></ComponentWrapper>;
