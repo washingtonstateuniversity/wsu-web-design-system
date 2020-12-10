63d7630a986d4f2a6fd63978e0edd5c723c3ee12
Nic Ford <nicdford@gmail.com>
2020-12-10 10:59:44 -0800

    Setup content tiles component

---
+++ packages/components/content-tiles/index.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { 
+	Repeater,
+	getUtilityClasses,
+	getMarginSpacingClasses
+} from '@wsuwebteam/build-tools/js/helpers';
+
+import {
+	WsuIcon
+} from '../index';
+
+import './style.scss';
+
+const ContentTiles = ( props, ref ) => {
+
+	console.log(props);
+
+	
+	let wrapperClasses = getUtilityClasses(
+		[
+			{ key: 'featuredTileLocation', prefix: 'wsu-c-', classSlug: 'tiles' },
+		],
+		props,
+		['wsu-c-tiles']
+	);
+
+	let tileContentClasses = getUtilityClasses(
+		[
+			{ key: 'overlayColor', prefix: 'wsu-c-tile__', classSlug: 'content' },
+		],
+		props,
+		['wsu-c-tile__content']
+	);
+
+	return (
+		<div className={ wrapperClasses }>
+			<Repeater numTimes={5}>
+				<div 
+					className="wsu-c-tile"
+					style={{
+						backgroundImage: "url('https://source.unsplash.com/collection/9042806')",
+						backgroundPositionX: '50%',
+						backgroundPositionY: '50%',
+					}}
+				>
+					<div className={ tileContentClasses }>
+						<div className="wsu-c-tile__heading">Butch Cougar <WsuIcon name="link" /></div>
+						<div className="wsu-c-tile__caption">Senior | Chemical Engineering | Edgewood, WA</div>
+					</div>
+				</div>
+			</Repeater>
+		</div>
+	);
+
+}
+
+
+ContentTiles.propTypes = {
+	/** Controls the overall aesthetic of the button */
+	style: PropTypes.string,
+	/** Text that displays on the button */
+	buttonText: PropTypes.string,
+	/** Destination for when the button is clicked */
+	buttonUrl: PropTypes.string,
+	/** Overall size of the button */
+	size: PropTypes.string,
+	/** Accepts any additional class names */
+	className: PropTypes.string,
+	/** Defines the shape of the button */
+	shape: PropTypes.oneOf(['default', 'round']),
+	/** Defines the color of the button */
+	color: PropTypes.oneOf(['default', 'white']),
+	/** Defines the html tag element for the button, defaults to anchor */
+	tag: PropTypes.string,
+	/** Accepts onClick function */
+	onClick: PropTypes.func,
+	/** Icon to be put before buttonText */
+	iconBefore: PropTypes.string,
+	/** Icon to be put after buttonText */
+	iconAfter: PropTypes.string,
+	/** Supports [ariaLabel](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_aria-label_attribute) attribute */
+	ariaLabel: PropTypes.string,
+	/** Supports [ariaLabeledBy](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_aria-labelledby_attribute) attribute */
+	ariaLabeledBy: PropTypes.string,
+}
+
+ContentTiles.defaultProps = {
+	style: 'default',
+	buttonText: '',
+	buttonUrl: '',
+	size: 'default',
+	className: '',
+	shape: 'default',
+	color: 'default',
+	tag: 'default',
+	onClick: null,
+	buttonAlign: 'default',
+	iconBefore: '',
+	iconAfter: '',
+	ariaLabel: '',
+	ariaLabeledBy: '',
+}
+
+export default ContentTiles;
---
+++ storybook/stories/content-tiles.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs, text, boolean, number, select } from "@storybook/addon-knobs";
+import { previewGroupID, optionsGroupID } from '@wsuwebteam/build-tools/js/helpers/storybook';
+
+// Storybook Imports
+import ComponentWrapper from '../../packages/component-parts/content-wrapper';
+
+// Component Imports
+import { WsuTiles } from '../../packages/components';
+
+// Story Time
+export default {
+	title: 'Components/Content/Tiles',
+	parameters: {
+		status: 'In Development' // In Development | Needs Feedback | Stable | Needs Deployment | Has Known Issues
+	},
+	decorators: [
+		withA11y,
+		withKnobs,
+	],
+	component: WsuTiles
+};
+
+export const DefaultTiles = () => {
+
+	let classes = ( boolean('Apply wsu-c-content class', false, previewGroupID ) ) ? ['wsu-c-content'] : '';
+	let layout = ( select('Wrapper Layout', ['full', 'content', 'centered'], 'content', previewGroupID ) );
+
+	return (
+		<ComponentWrapper layout={ layout } className={ classes }>
+			<WsuTiles 
+				overlayColor={ select( 'overlayColor', ['default', 'crimson'], 'default', optionsGroupID )  }
+				featuredTileLocation={ select( 'featuredTileLocation', ['default', 'right', 'center'], 'default', optionsGroupID )  }
+			/>
+		</ComponentWrapper>
+	)
+}
